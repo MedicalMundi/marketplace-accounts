01e2c7e346f0c047406ffd28e480fb4dd8ad7041
zerai <teclaizerai@gmail.com>
2024-02-17 12:55:48 +0100

    Security: added verify email procedure.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -17,6 +17,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationFormType;
+use App\Repository\UserRepository;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -24,11 +25,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Uid\UuidV4;
+use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
+use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
 class RegistrationController extends AbstractController
 {
     #[Route('/register', name: 'app_register')]
-    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager): Response
+    public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, VerifyEmailHelperInterface $verifyEmailHelper): Response
     {
         $user = new User();
         $form = $this->createForm(RegistrationFormType::class, $user);
@@ -47,6 +50,19 @@ class RegistrationController extends AbstractController
 
             $entityManager->persist($user);
             $entityManager->flush();
+
+            $signatureComponent = $verifyEmailHelper->generateSignature(
+                'app_verify_email',
+                (string) $user->getId(),
+                $user->getEmail(),
+                [
+                    'id' => (string) $user->getId(),
+                ]
+            );
+
+            // TEMP: REMOVE LATER
+            $this->addFlash('success', 'Confirm your email at: ' . $signatureComponent->getSignedUrl());
+
             // do anything else you need here, like send an email
             //TODO SEND VERIFY MAIL
 
@@ -56,5 +72,41 @@ class RegistrationController extends AbstractController
         return $this->render('registration/register.html.twig', [
             'registrationForm' => $form->createView(),
         ]);
+    }
+
+    #[Route('/verify', name: 'app_verify_email')]
+    public function verifyUserEmail(Request $request, VerifyEmailHelperInterface $verifyEmailHelper, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
+    {
+        $user = $userRepository->find($request->query->get('id'));
+
+        if (! $user) {
+            throw $this->createNotFoundException();
+        }
+
+        try {
+            $verifyEmailHelper->validateEmailConfirmation(
+                $request->getUri(),
+                (string) $user->getId(),
+                $user->getEmail()
+            );
+        } catch (VerifyEmailExceptionInterface $e) {
+            $this->addFlash('error', $e->getReason());
+
+            return $this->redirectToRoute('app_register');
+        }
+
+        $user->setIsVerified(true);
+
+        $entityManager->flush();
+
+        $this->addFlash('success', 'Account verified! You can now login.');
+
+        return $this->redirectToRoute('app_login');
+    }
+
+    #[Route('/verify/resend', name: 'app_verify_resend_email')]
+    public function resendVerifyEmail(): Response
+    {
+        return $this->render('registration/resend_verify_email.html.twig');
     }
 }
