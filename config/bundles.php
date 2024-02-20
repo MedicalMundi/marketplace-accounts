734fe08be812ab6fdca2ce7d104a47080a13f324
zerai <teclaizerai@gmail.com>
2024-02-20 18:12:22 +0100

    Composer: added fixture-bundle and zenstruck/foundry.

--- config/bundles.php
+++ config/bundles.php
@@ -17,4 +17,6 @@ return [
     SymfonyCasts\Bundle\VerifyEmail\SymfonyCastsVerifyEmailBundle::class => ['all' => true],
     Knp\Bundle\MenuBundle\KnpMenuBundle::class => ['all' => true],
     BabDev\PagerfantaBundle\BabDevPagerfantaBundle::class => ['all' => true],
+    Zenstruck\Foundry\ZenstruckFoundryBundle::class => ['dev' => true, 'test' => true],
+    Doctrine\Bundle\FixturesBundle\DoctrineFixturesBundle::class => ['dev' => true, 'test' => true],
 ];
