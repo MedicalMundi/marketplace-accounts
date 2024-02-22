19a9a472c9a8afe12564f0c2c62b919dfb7c778c
zerai <teclaizerai@gmail.com>
2024-02-22 21:50:32 +0100

    Improve admin UI.

---
+++ _iam/src/AdapterForReadingOauthClients/OauthClientDataProvider.php
@@ -0,0 +1,64 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the medicalmundi/marketplace-engine
+ *
+ * @copyright (c) 2023 MedicalMundi
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * {@link https://github.com/medicalmundi/marketplace-accounts/graphs/contributors developer} and is licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * @license https://github.com/MedicalMundi/marketplace-accounts/blob/main/LICENSE MIT
+ */
+
+namespace IdentityAccess\AdapterForReadingOauthClients;
+
+use Doctrine\DBAL\Connection;
+use Ecotone\Modelling\Attribute\QueryHandler;
+use IdentityAccess\Core\GetOauthClientByIdentifierQuery;
+use IdentityAccess\Core\ShowAllOauthClientQuery;
+
+class OauthClientDataProvider
+{
+    public function __construct(
+        private readonly Connection $connection
+    ) {
+    }
+
+    #[QueryHandler]
+    public function showAllClients(ShowAllOauthClientQuery $query): array
+    {
+        try {
+            $result = $this->connection->executeQuery(
+                <<<SQL
+    SELECT * FROM oauth2_client
+SQL
+            )->fetchAllAssociative();
+            // TODO: remove exception
+        } catch (\Exception) {
+            return [];
+        }
+        return $result;
+    }
+
+    #[QueryHandler]
+    public function getOauthClientByIdentifier(GetOauthClientByIdentifierQuery $query): array
+    {
+        try {
+            $result = $this->connection->executeQuery(
+                <<<SQL
+    SELECT * FROM oauth2_client WHERE identifier = :identifier
+SQL,
+                [
+                    'identifier' => $query->identifier,
+                ]
+            )->fetchAllAssociative()[0];
+            // TODO: remove exception
+        } catch (\Exception) {
+            return [];
+        }
+        return $result;
+    }
+}
--- tools/phparkitect/VendorDependencies/allowed_in_iam_adapters.php
+++ tools/phparkitect/VendorDependencies/allowed_in_iam_adapters.php
@@ -5,30 +5,11 @@ return [
 
     'Ecotone\Modelling\QueryBus',
 
+    'Doctrine\DBAL\Connection',
+
     'Symfony\Bundle\FrameworkBundle\Controller\AbstractController',
     'Symfony\Component\HttpFoundation\Request',
     'Symfony\Component\HttpFoundation\Response',
-//    /** ALL EXCEPTIONS */
-//    'Symfony\Component\HttpKernel\Exception\*',
-//    'Symfony\Component\HttpKernel\Attribute\AsController',
-//
-//    'Symfony\Component\Security\Http\Authentication\AuthenticationUtils',
-//
-//    'Symfony\Component\RateLimiter\RateLimiterFactory',
-//
-//    'Symfony\Component\OptionsResolver\OptionsResolver',
-//
-//    'Symfony\Component\Mailer\MailerInterface',
-//    'Symfony\Component\Mime\Email',
-//
-//    'Symfony\Component\Form\FormBuilderInterface',
-//    'Symfony\Component\Form\AbstractType',
-//    'Symfony\Component\Form\FormError',
-//    /** ALL FORM TYPE */
-//    'Symfony\Component\Form\Extension\Core\Type\*',
-//
-//    /** ALL CONSTRAINTS */
-//    'Symfony\Component\Validator\Constraints\*',
-//
-//    'Symfony\Contracts\Translation\TranslatorInterface',
+
+    'Symfony\Component\Routing\Annotation\Route',
 ];
