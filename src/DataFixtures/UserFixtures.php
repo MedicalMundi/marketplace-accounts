bc25d77e1bf56a76c95bd0796713f84f84a89d57
zerai <teclaizerai@gmail.com>
2024-02-23 09:33:11 +0100

    Setup dev ficture.

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,34 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the medicalmundi/marketplace-accounts
+ *
+ * @copyright (c) 2023 MedicalMundi
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * {@link https://github.com/medicalmundi/marketplace-accounts/graphs/contributors developer} and is licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * @license https://github.com/MedicalMundi/marketplace-accounts/blob/main/LICENSE MIT
+ */
+
+namespace App\DataFixtures;
+
+use App\Tests\Fixture\Factory\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Bundle\FixturesBundle\FixtureGroupInterface;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture implements FixtureGroupInterface
+{
+    public function load(ObjectManager $manager): void
+    {
+        UserFactory::createMany(10);
+    }
+
+    public static function getGroups(): array
+    {
+        return ['dev', 'dev-user'];
+    }
+}
