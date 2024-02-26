5d0cf8fe0eb2473891f624f8ed572950f5317be2
zerai <teclaizerai@gmail.com>
2024-02-26 19:10:55 +0100

    Improve admin ui.

---
+++ _iam/src/AdapterForWeb/Administration/OauthClient/Form/Dto/OauthClientDto.php
@@ -0,0 +1,35 @@
+<?php declare(strict_types=1);
+
+/*
+ * This file is part of the medicalmundi/marketplace-accounts
+ *
+ * @copyright (c) 2023 MedicalMundi
+ *
+ * This software consists of voluntary contributions made by many individuals
+ * {@link https://github.com/medicalmundi/marketplace-accounts/graphs/contributors developer} and is licensed under the MIT license.
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ * @license https://github.com/MedicalMundi/marketplace-accounts/blob/main/LICENSE MIT
+ */
+
+namespace IdentityAccess\AdapterForWeb\Administration\OauthClient\Form\Dto;
+
+class OauthClientDto
+{
+    public string $name;
+
+    public string $identifier;
+
+    public string $secret;
+
+    public array $redirectUris = [];
+
+    public array $grants = [];
+
+    public array $scopes = [];
+
+    public bool $active = false;
+
+    public bool $allowPlainTextPkce = false;
+}
--- tools/phparkitect/VendorDependencies/allowed_in_iam_adapters.php
+++ tools/phparkitect/VendorDependencies/allowed_in_iam_adapters.php
@@ -2,14 +2,21 @@
 
 return [
     'IdentityAccess\Core',
+    'IdentityAccess\Adapter*',
 
     'Ecotone\Modelling\QueryBus',
+
+    'League\Bundle\OAuth2ServerBundle',
 
     'Doctrine\DBAL\Connection',
 
     'Symfony\Bundle\FrameworkBundle\Controller\AbstractController',
     'Symfony\Component\HttpFoundation\Request',
     'Symfony\Component\HttpFoundation\Response',
+
+    'Symfony\Component\Form',
+
+    'Symfony\Component\OptionsResolver\OptionsResolver',
 
     'Symfony\Component\Routing\Annotation\Route',
 ];
