e9143888df9fdce61ab2617e8b9ac79f968291bd
zerai <teclaizerai@gmail.com>
2024-03-06 16:38:04 +0100

    Security: Authenticator set remember me always.

--- src/Security/Authenticator/OeModulesLoginFormAuthenticator.php
+++ src/Security/Authenticator/OeModulesLoginFormAuthenticator.php
@@ -23,12 +23,12 @@ use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\User\PasswordUpgraderInterface;
 use Symfony\Component\Security\Core\User\UserProviderInterface;
 use Symfony\Component\Security\Http\Authenticator\AbstractLoginFormAuthenticator;
+use Symfony\Component\Security\Http\Authenticator\Passport\Badge\CsrfTokenBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\PasswordUpgradeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\RememberMeBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Badge\UserBadge;
 use Symfony\Component\Security\Http\Authenticator\Passport\Credentials\PasswordCredentials;
 use Symfony\Component\Security\Http\Authenticator\Passport\Passport;
-use Symfony\Component\Security\Http\ParameterBagUtils;
 use Symfony\Component\Security\Http\SecurityRequestAttributes;
 use Symfony\Component\Security\Http\Util\TargetPathTrait;
 
@@ -53,6 +53,7 @@ class OeModulesLoginFormAuthenticator extends AbstractLoginFormAuthenticator
 
         $userBadge = new UserBadge((string) $credentials['username'], $this->userProvider->loadUserByIdentifier(...));
         $passport = new Passport($userBadge, new PasswordCredentials((string) $credentials['password']), [new RememberMeBadge()]);
+        $passport->addBadge(new CsrfTokenBadge('authenticate', (string) $credentials['csrf_token']));
 
         if ($this->userProvider instanceof PasswordUpgraderInterface) {
             $passport->addBadge(new PasswordUpgradeBadge((string) $credentials['password'], $this->userProvider));
@@ -73,12 +74,9 @@ class OeModulesLoginFormAuthenticator extends AbstractLoginFormAuthenticator
     private function getCredentials(Request $request): array
     {
         $credentials = [];
-        //TODO
-        //$credentials['csrf_token'] = ParameterBagUtils::getRequestParameterValue($request, $this->options['csrf_parameter']);
-
+        $credentials['csrf_token'] = (string) $request->request->get('_csrf_token', '');
         $credentials['username'] = (string) $request->request->get('_username', '');
         $credentials['password'] = (string) $request->request->get('_password', '');
-
         $credentials['username'] = trim($credentials['username']);
 
         $request->getSession()->set(SecurityRequestAttributes::LAST_USERNAME, $credentials['username']);
