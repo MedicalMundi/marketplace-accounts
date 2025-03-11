2f8f1fbd09cc292b90545a23059f8b441cf39ad1
zerai <teclaizerai@gmail.com>
2025-03-11 13:38:51 +0100

    Update verify email procedure, removed deprecation.

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -18,20 +18,29 @@ namespace App\Controller;
 use App\Entity\User;
 use App\Form\RegistrationFormType;
 use App\Repository\UserRepository;
+use App\Security\EmailVerifier;
 use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Mailer\MailerInterface;
+use Symfony\Component\Mime\Address;
 use Symfony\Component\Mime\Email;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Uid\UuidV4;
+use Symfony\Contracts\Translation\TranslatorInterface;
 use SymfonyCasts\Bundle\VerifyEmail\Exception\VerifyEmailExceptionInterface;
 use SymfonyCasts\Bundle\VerifyEmail\VerifyEmailHelperInterface;
 
 class RegistrationController extends AbstractController
 {
+    public function __construct(
+        private EmailVerifier $emailVerifier
+    ) {
+    }
+
     #[Route('/register', name: 'app_register')]
     public function register(Request $request, UserPasswordHasherInterface $userPasswordHasher, EntityManagerInterface $entityManager, VerifyEmailHelperInterface $verifyEmailHelper, MailerInterface $mailer): Response
     {
@@ -61,16 +70,30 @@ class RegistrationController extends AbstractController
             $entityManager->persist($user);
             $entityManager->flush();
 
-            $signatureComponent = $verifyEmailHelper->generateSignature(
+            // deprecated
+            //            $signatureComponent = $verifyEmailHelper->generateSignature(
+            //                'app_verify_email',
+            //                (string) $user->getId(),
+            //                $user->getEmail(),
+            //                [
+            //                    'id' => (string) $user->getId(),
+            //                ]
+            //            );
+
+            // deprecated
+            //$this->sendConfirmationEmail($mailer, $signatureComponent->getSignedUrl(), $user->getEmail());
+
+            // generate a signed url and email it to the user
+            $this->emailVerifier->sendEmailConfirmation(
                 'app_verify_email',
-                (string) $user->getId(),
-                $user->getEmail(),
-                [
-                    'id' => (string) $user->getId(),
-                ]
+                $user,
+                (new TemplatedEmail())
+                    ->from(new Address('mailer@openemrmarketplace.com', 'OpenemrMarketplaceMailBot'))
+                    ->to($user->getEmail())
+                    ->subject('Please Confirm your Email')
+                    ->htmlTemplate('registration/confirmation_email.html.twig')
             );
 
-            $this->sendConfirmationEmail($mailer, $signatureComponent->getSignedUrl(), $user->getEmail());
             $this->addFlash('info', 'Please check your mail and confirm your email address!');
 
             return $this->redirectToRoute('app_login');
@@ -80,35 +103,55 @@ class RegistrationController extends AbstractController
             'registrationForm' => $form->createView(),
         ]);
     }
+    //  deprecated
+    //    #[Route('/verify', name: 'app_verify_email')]
+    //    public function verifyUserEmail(Request $request, VerifyEmailHelperInterface $verifyEmailHelper, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
+    //    {
+    //        $user = $userRepository->find($request->query->get('id'));
+    //
+    //        if (! $user) {
+    //            throw $this->createNotFoundException();
+    //        }
+    //
+    //        try {
+    //            $verifyEmailHelper->validateEmailConfirmation(
+    //                $request->getUri(),
+    //                (string) $user->getId(),
+    //                $user->getEmail()
+    //            );
+    //        } catch (VerifyEmailExceptionInterface $e) {
+    //            $this->addFlash('error', $e->getReason());
+    //
+    //            return $this->redirectToRoute('app_register');
+    //        }
+    //
+    //        $user->setIsVerified(true);
+    //
+    //        $entityManager->flush();
+    //
+    //        $this->addFlash('success', 'Account verified! You can now login.');
+    //
+    //        return $this->redirectToRoute('app_login');
+    //    }
 
-    #[Route('/verify', name: 'app_verify_email')]
-    public function verifyUserEmail(Request $request, VerifyEmailHelperInterface $verifyEmailHelper, UserRepository $userRepository, EntityManagerInterface $entityManager): Response
+    #[Route('/verify/email', name: 'app_verify_email')]
+    public function verifyUserEmail(Request $request, TranslatorInterface $translator): Response
     {
-        $user = $userRepository->find($request->query->get('id'));
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
 
-        if (! $user) {
-            throw $this->createNotFoundException();
-        }
-
+        // validate email confirmation link, sets User::isVerified=true and persists
         try {
-            $verifyEmailHelper->validateEmailConfirmation(
-                $request->getUri(),
-                (string) $user->getId(),
-                $user->getEmail()
-            );
-        } catch (VerifyEmailExceptionInterface $e) {
-            $this->addFlash('error', $e->getReason());
+            $this->emailVerifier->handleEmailConfirmation($request, $this->getUser());
+        } catch (VerifyEmailExceptionInterface $exception) {
+            $this->addFlash('verify_email_error', $translator->trans($exception->getReason(), [], 'VerifyEmailBundle'));
 
             return $this->redirectToRoute('app_register');
         }
 
-        $user->setIsVerified(true);
+        // @TODO Change the redirect on success and handle or remove the flash message in your templates
+        $this->addFlash('success', 'Your email address has been verified.');
 
-        $entityManager->flush();
-
-        $this->addFlash('success', 'Account verified! You can now login.');
-
-        return $this->redirectToRoute('app_login');
+        return $this->redirectToRoute('app_register');
     }
 
     #[Route('/verify/resend', name: 'app_verify_resend_email')]
