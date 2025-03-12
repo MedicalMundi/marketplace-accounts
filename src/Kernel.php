1fb3bbb386c3cb8c3d059f950a119add56f5e287
zero <teclaizerai@gmail.com>
2025-03-12 09:46:27 +0100

    refactoring iam to module (#169)
    * Refactor IdentityAccess namespace/folder.
    
    * Update tools configuration (new iam folder).
    
    * Configure module as symfony extension.

--- src/Kernel.php
+++ src/Kernel.php
@@ -2,10 +2,17 @@
 
 namespace App;
 
+use IdentityAccess\Infrastructure\Framework\Extension\IdentityAccessModuleExtension;
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
+use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
 
 class Kernel extends BaseKernel
 {
     use MicroKernelTrait;
+
+    protected function build(ContainerBuilder $container): void
+    {
+        $container->registerExtension(new IdentityAccessModuleExtension());
+    }
 }
